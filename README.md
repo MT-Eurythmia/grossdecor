e1af2fa9a0c010095e6d943502483318ed74a09c
Grossam <lejimi1972@free.fr>
2017-06-03 17:49:17 +0200

    Computer added

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 An attempt to add mesh based node often needed to make a build more "inhabited"
 
+![](capture.png "Two done !")
+
 TODO :
 
 Mural clock
 BBQ
-Vintage computer (with big tapes)
+
--- init.lua
+++ init.lua
@@ -3,16 +3,32 @@ minetest.register_node('grossdecor:cash_register', {
 	tiles = {'grossdecor_cash_register.png'},
 	drawtype = 'mesh',
 	mesh = 'grossdecor_cash_register.obj',
-	groups = {level = 1},
+	groups = {oddly_breakable_by_hand = 1},
 	paramtype = 'light',
 	paramtype2 = 'facedir',
 	is_ground_content = false,
 	selection_box = {
 		type = "fixed",
 		fixed = {{-.5, -.5, -.5, .5, -.125, .5}},
-		},
+	},
 	collision_box = {
 		type = "fixed",
 		fixed = {{-.5, -.5, -.5, .5, -.125, .5}},
-		}
+	}
+})
+
+
+minetest.register_node('grossdecor:vintage_computer', {
+	description = 'Vintage computer',
+	drawtype = 'mesh',
+	mesh = 'grossdecor_vintage_computer.obj',
+	tiles = {'grossdecor_vintage_computer.png'},
+	groups = {oddly_breakable_by_hand = 1},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	is_ground_content = false,
+	selection_box = {
+		type = 'fixed',
+		fixed = {{-.5, -.5, -.5, .5, 1, .5}},
+	}
 })
